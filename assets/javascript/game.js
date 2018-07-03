666ff39b4d2cf6d5f66abe4bdfff9dcb87d2c73e
Tals <natalia.mahabir@gmail.com>
2018-07-03 02:58:22 -0400

    JS added, basic function of the game added

---
+++ assets/javascript/game.js
@@ -0,0 +1,94 @@
+// Make sure the document has loaded
+$(document).ready();
+
+// Variables 
+var randomNumber = Math.floor(Math.random()*102)+19;
+var pooh = Math.floor(Math.random()*12)+1;
+var eeyore = Math.floor(Math.random()*12)+1;
+var tigger = Math.floor(Math.random()*12)+1;
+var piglet = Math.floor(Math.random()*12)+1;
+var myScore = 0;
+var wins = 0;
+var losses = 0;
+
+// Resets
+function reset() {
+    randomNumber = Math.floor(Math.random()*102)+19;
+    $("#randomNumber").text(randomNumber);
+    pooh = Math.floor(Math.random()*12)+1;
+    eeyore = Math.floor(Math.random()*12)+1;
+    tigger = Math.floor(Math.random()*12)+1;
+    piglet = Math.floor(Math.random()*12)+1;
+    myScore = 0;
+    $("#yourScore").text(myScore);
+}
+
+// Game 
+// I'm pretty sure the code below can be shortened in a loop or something, but I haven't placed the loop as yet.
+$("#randomNumber").text(randomNumber);
+
+$("#Pooh").click(function(){
+    $("#Pooh").attr("poohValue", pooh);
+    console.log($(this).attr("poohValue"));
+    myScore += parseInt($(this).attr("poohValue"));
+    $("#yourScore").text(myScore);
+    if (myScore === randomNumber){
+        wins++;
+        $("#wins").html("Wins: " + wins);
+        reset();        
+    }    
+    else if (myScore > randomNumber){
+        losses++;
+        $("#losses").html("Losses: " + wins);        
+        reset();
+    }
+})
+$("#Eeyore").click(function(){
+    $("#Eeyore").attr("eeyoreValue", eeyore);
+    console.log($(this).attr("eeyoreValue"));
+    myScore += parseInt($(this).attr("eeyoreValue"));
+    $("#yourScore").text(myScore);
+    if (myScore === randomNumber){
+        wins++;
+        $("#wins").html("Wins: " + wins);        
+        reset();
+    }    
+    else if (myScore > randomNumber){
+        losses++;
+        $("#losses").html("Losses: " + wins);        
+        reset();
+    }
+})
+$("#Tigger").click(function(){
+    $("#Tigger").attr("tiggerValue", tigger);
+    console.log($(this).attr("tiggerValue"));
+    myScore += parseInt($(this).attr("tiggerValue"));
+    $("#yourScore").text(myScore);
+    if (myScore === randomNumber){
+        wins++;
+        $("#wins").html("Wins: " + wins);        
+        reset();
+    }    
+    else if (myScore > randomNumber){
+        losses++;
+        $("#losses").html("Losses: " + wins);        
+        reset();
+    }
+})
+$("#Piglet").click(function(){
+    $("#Piglet").attr("pigletValue", piglet);
+    console.log($(this).attr("pigletValue"));
+    myScore += parseInt($(this).attr("pigletValue"));
+    $("#yourScore").text(myScore);
+    if (myScore === randomNumber){
+        wins++;
+        $("#wins").html("Wins: " + wins);        
+        reset();
+    }    
+    else if (myScore > randomNumber){
+        losses++;
+        $("#losses").html("Losses: " + wins);        
+        reset();
+    }
+})
+
